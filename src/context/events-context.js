06f0ab69f5dd95133c31fa2b1efe09e396cf909d
CesarRivasP <cesarjose58@gmail.com>
2019-06-05 17:51:17 -0400

    Added url and chance of state and implemented EventsContext.Provider

--- src/context/events-context.js
+++ src/context/events-context.js
@@ -8,18 +8,29 @@ export const EventsConsumer = EventsContext.Consumer;
 
 
 class EventsProvider extends Component {
+  state = {
+    events: []
+  }
 
   token = 'NLC37RG4EVX2O4QXAGHJ';
-  sort ='date'
+  sortBy ='date';
 
   getEvents = async (search) => {
-    let url = `https://www.eventbriteapi.com/v3/events/search/?q=${search.name}&
-                categories=${search.categorie}&sort_by=${this.sort}&token=${this.token}&locale=es_ES`
+    let url = `https://www.eventbriteapi.com/v3/events/search/?q=${search.nameEvent}&categories=${search.categoryEvent}&sort_by=${this.sortBy}&token=${this.token}&locale=es_ES`;
+
+    // consultar la api con la url
+    const events = await axios.get(url);
+
+    this.setState({
+      events: events.data.events
+    })
   }
 
   render(){
     return (
-
+      <EventsContext.Provider value={{ events: this.state.events, getEvents: this.getEvents }}>
+        {this.props.children}
+      </EventsContext.Provider>
     );
   }
 }
