c279beb35fe635ec1daa29ee4a3d340718f1b88e
CesarRivasP <cesarjose58@gmail.com>
2019-06-05 16:28:10 -0400

    Added function getEventData for get values of inputs

--- src/components/form.js
+++ src/components/form.js
@@ -8,9 +8,17 @@ class Form extends Component {
     categoryEvent: ''
   }
 
+  // Si el usuario agrega un evento o categoria
+  getEventData = (e) => {
+    this.setState({
+      // Para que esto funciones el nombre del state tiene que coincidir con el del input
+      [e.target.name]: e.target.value
+    })
+  }
+
   render(){
     return (
-      <form >
+      <form>
         <fieldset className="uk-fieldset uk-margin">
           <legend className="uk-legend uk-text-center">
             Busca tu evento por Nombre o Categoría
@@ -19,10 +27,17 @@ class Form extends Component {
 
         <div className="uk-column-1-3@m uk-margin">
           <div className="uk-margin" uk-margin="true">
-            <input name="nameEvent" className="uk-input" type="text" placeholder="Nombre de Evento o Ciudad" />
+            <input
+              name="nameEvent"
+              className="uk-input"
+              type="text"
+              placeholder="Nombre de Evento o Ciudad"
+              onChange={this.getEventData}
+            />
           </div>
             <div className="uk-margin" uk-margin="true">
-              <select name="categoryEvent" className="uk-select">
+              <select name="categoryEvent" className="uk-select" onChange={this.getEventData}>
+                <option value="">Seleccione Categoría</option>
                 <CategoriesConsumer>
                   {/* Aqui adentro es donde se va a tener acceso a los datos de este consumer */}
                   {/* se accede a los valores como value */}
