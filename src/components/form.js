b43cf454ca999fbb04d8b6945db5ea18e4cfc33e
CesarRivasP <cesarjose58@gmail.com>
2019-06-05 18:28:16 -0400

    added css classes

--- src/components/form.js
+++ src/components/form.js
@@ -19,59 +19,61 @@ class Form extends Component {
 
   render(){
     return (
-      <EventsConsumer>
-        {
-          (value) => {
-            console.log(value)
-            return (
+      <div className="form-container">
+        <EventsConsumer>
+          {
+            (value) => {
+              console.log(value)
+              return (
 
-              <form onSubmit={(e) => {
+                <form onSubmit={(e) => {
                   e.preventDefault();
                   value.getEvents(this.state)
                 }}>
-                <fieldset className="uk-fieldset uk-margin">
-                  <legend className="uk-legend uk-text-center">
-                    Busca tu evento por Nombre o Categoría
-                  </legend>
-                </fieldset>
+                    <fieldset className="uk-fieldset uk-margin">
+                      <legend className="uk-legend uk-text-center">
+                        Busca tu evento por nombre o categoría
+                      </legend>
+                    </fieldset>
 
-                <div className="uk-column-1-3@m uk-margin">
-                  <div className="uk-margin" uk-margin="true">
-                    <input
-                      name="nameEvent"
-                      className="uk-input"
-                      type="text"
-                      placeholder="Nombre de Evento o Ciudad"
-                      onChange={this.getEventData}
-                    />
+                    <div className="uk-column-1-3@m uk-margin">
+                      <div className="uk-margin" uk-margin="true">
+                        <input
+                          name="nameEvent"
+                          className="uk-input"
+                          type="text"
+                          placeholder="Nombre de Evento o Ciudad"
+                          onChange={this.getEventData}
+                        />
+                      </div>
+                      <div className="uk-margin" uk-margin="true">
+                        <select name="categoryEvent" className="uk-select" onChange={this.getEventData}>
+                          <option value="">Seleccione Categoría</option>
+                          <CategoriesConsumer>
+                            {/* Aqui adentro es donde se va a tener acceso a los datos de este consumer */}
+                            {/* se accede a los valores como value */}
+                            { (value) => {
+                              return (
+                                value.categories.map((categorie) => (
+                                  <option key={categorie.id} value={categorie.id}>{categorie.name_localized}</option>
+                                ))
+                              )
+                            }
+                          }
+                        </CategoriesConsumer>
+                      </select>
+                    </div>
+
+                    <div>
+                      <input type="submit" className="uk-button submit-button" value="Busca Eventos" />
+                    </div>
                   </div>
-                  <div className="uk-margin" uk-margin="true">
-                    <select name="categoryEvent" className="uk-select" onChange={this.getEventData}>
-                      <option value="">Seleccione Categoría</option>
-                      <CategoriesConsumer>
-                        {/* Aqui adentro es donde se va a tener acceso a los datos de este consumer */}
-                        {/* se accede a los valores como value */}
-                        { (value) => {
-                          return (
-                            value.categories.map((categorie) => (
-                              <option key={categorie.id} value={categorie.id}>{categorie.name_localized}</option>
-                            ))
-                          )
-                        }
-                      }
-                    </CategoriesConsumer>
-                  </select>
-                </div>
-
-                <div>
-                  <input type="submit" className="uk-button submit-button" value="Busca Eventos" />
-                </div>
-              </div>
-            </form>
-            )
+                </form>
+              )
+            }
           }
-        }
-      </EventsConsumer>
+        </EventsConsumer>
+      </div>
     );
   }
 }
