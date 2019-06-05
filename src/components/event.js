03ceebb16d7dd22a1daed9da72d0343e8e1e5a50
CesarRivasP <cesarjose58@gmail.com>
2019-06-05 18:27:43 -0400

    Implemented event component

--- src/components/event.js
+++ src/components/event.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
-const Event = () => (
-  <div className="uk-child-width-1-3@m" uk-grid="true">
-    <h1>Resultados</h1>
+const Event = ({event}) => {
+  // extraer el texto
+  let { text } = event.description;
 
-  </div>
-);
+  if(text){
+    if(text.length > 250){
+      text = text.substr(0, 250)
+    }
+  }
+  else {
+    text = null;
+  }
+
+  return (
+    <div>
+      <div className="uk-card uk-card-default">
+        <div className="uk-card-media-top">
+          {
+            event.logo &&
+            <img src={event.logo.url} alt={event.name} />
+          }
+        </div>
+
+        <div className="uk-card-body">
+          <h3 className="uk-card-title">
+            {event.name.text}
+          </h3>
+          <p className="description-event">
+            { text }
+          </p>
+        </div>
+
+        <div className="uk-card-footer">
+          <a target="_blank" rel="noopener noreferrer" href={event.url} className="uk-button info-button">Mas Información</a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Event;
