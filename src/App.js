6d8e95bdc15f31c07af7cbcde4a0d643bd1912fb
CesarRivasP <cesarjose58@gmail.com>
2019-06-05 17:50:34 -0400

    Implemented EventsProvider context

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Fragment } from 'react';
 import Header from './components/header';
 import Form from './components/form';
 import CategoriesProvider from './context/categories-context';
+import EventsProvider from './context/events-context';
 
 
 function App() {
   return (
-    <CategoriesProvider>
+    <Fragment>
       <Header />
-      <div className="uk-container">
-        <Form />
-      </div>
-    </CategoriesProvider>
-  );
+
+      <EventsProvider>
+        <CategoriesProvider>
+          <div className="uk-container">
+            <Form />
+          </div>
+        </CategoriesProvider>
+      </EventsProvider>
+    </Fragment>
+    );
 }
 
 export default App;
